ff25ee1b7c5ad5c14ffaf4302297a87f0d4fbf64
Dima Pustakhod <dima.pu@gmail.com>
2023-08-21 01:10:03 +0200

    Add distances calculation

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
-# Read data
+# %% Read data
 raw_data_fn = Path(__file__).parent / "_data/raw.xlsx"
 df_bouys: pd.DataFrame = pd.read_excel(raw_data_fn, "buoys")
-df_stretches = pd.read_excel(raw_data_fn, "stretches")
+df_stretches: pd.DataFrame = pd.read_excel(raw_data_fn, "stretches")
 
 
 def convert_coordinate(c: str) -> float:
@@ -24,7 +26,7 @@ def convert_coordinate(c: str) -> float:
     return int(deg) + float(minutes) / 60.0
 
 
-# Normalize data
+# %% Normalize data
 df_bouys["name"] = (
     df_bouys["Name"].str.replace(" ", "").str.replace("-", "").str.upper()
 )
@@ -35,33 +37,58 @@ df_bouys = df_bouys[["Zone", "lat", "lon"]]
 
 df_stretches["start"] = df_stretches["Start"].str.replace("-", "").str.upper()
 df_stretches["end"] = df_stretches["Eind"].str.replace("-", "").str.upper()
+df_stretches["Afstand"] = (
+    df_stretches["Afstand"].str.replace(",", ".").astype(float)
+)
 df_stretches = df_stretches[["Afstand", "Max Aantal", "type", "start", "end"]]
 
 
-# Plot points
+# %% Get edge coordinates and distances, start and end
+df_stretches = pd.merge(
+    df_stretches,
+    df_bouys,
+    left_on="start",
+    right_on="name",
+    how="left",
+)
+df_stretches = pd.merge(
+    df_stretches,
+    df_bouys,
+    left_on="end",
+    right_on="name",
+    suffixes=["_start", "_end"],
+    how="left",
+)
+df_stretches["dx"] = (df_stretches["lon_start"] - df_stretches["lon_end"]) * 60
+df_stretches["dy"] = (df_stretches["lat_start"] - df_stretches["lat_end"]) * 60
+df_stretches["dist"] = np.sqrt(
+    (df_stretches["dx"] * 0.6) ** 2 + df_stretches["dy"] ** 2
+)
+df_stretches["distance_diff"] = df_stretches["Afstand"] - df_stretches["dist"]
+
+
+# %% Plots
+df_stretches_plot = df_stretches.dropna(axis="index")
+n_total = len(df_stretches)
+n_found = len(df_stretches_plot)
+
+print(f"{n_total=}, {n_found=}, n_not_found={n_total-n_found}")
+
+# Plot points and edges 
 ax = sns.scatterplot(data=df_bouys, x="lon", y="lat", style="Zone", hue="Zone")
 ax.set_aspect(1 / 0.6)
 
-# Plot edges
-n_found, n_not_found = 0, 0
-for i, edge in df_stretches.iterrows():
-    start = edge["start"]
-    end = edge["end"]
-    if start not in df_bouys.index:
-        print(f"missing start {start}")
-        n_not_found += 1
-        continue
-
-    if end not in df_bouys.index:
-        print(f"missing end {end}")
-        n_not_found += 1
-        continue
-
-    n_found += 1
-
-    sx, sy = df_bouys.loc[start]["lon"], df_bouys.loc[start]["lat"]
-    ex, ey = df_bouys.loc[end]["lon"], df_bouys.loc[end]["lat"]
-
+for i, r in df_stretches_plot.iterrows():
+    sx, sy = r["lon_start"], r["lat_start"]
+    ex, ey = r["lon_end"], r["lat_end"]
     ax.plot([sx, ex], [sy, ey])
 
-print(f"{n_found=}, {n_not_found=}")
+# Plot difference between calculated and official distance
+plt.figure()
+ax = sns.scatterplot(
+    data=df_stretches_plot,
+    x=df_stretches_plot.index,
+    y="distance_diff",
+    style="Zone_start",
+    hue="Zone_end",
+)
