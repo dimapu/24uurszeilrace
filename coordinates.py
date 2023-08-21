977c6c27644be216cc822c0d6f5bb445cc623424
Dima Pustakhod <dima.pu@gmail.com>
2023-08-22 01:59:31 +0200

    Move coordinate calculation to a separate file

---
+++ coordinates.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def convert_coordinate(c: str) -> float:
+    """Convert coordinate string to float.
+
+    Input format: "52°30,0000' N"
+    """
+    new = (
+        c.replace("°", " ")
+        .replace("'", " ")
+        .replace('"', " ")
+        .replace(",", ".")
+    )
+    deg, minutes, direction = new.split()
+    return int(deg) + float(minutes) / 60.0
+
+
+def get_direction(dx: float, dy: float) -> float:
+    """Return bearing from 0 to 360 degree.
+
+    dx and dy are corrdinate differences, end_point - start_point.
+
+    0 is when the second point is to the north, i.e. dy > 0, dx == 0
+    90 is when the second point is to the east, i.e. dy == 0, dx > 0,
+    etc.
+    """
+    return ((np.arctan2(dx, dy) / np.pi * 180) + 360) % 360
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
