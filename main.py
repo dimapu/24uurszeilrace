029d8f3438c15338bfcdd561409fa287cc3216bd
Dima Pustakhod <dima.pu@gmail.com>
2023-08-22 02:00:10 +0200

    Add direction calculations

--- main.py
+++ main.py
@@ -5,28 +5,17 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from coordinates import convert_coordinate, get_direction
+from speed import get_speed
+
 LON_FACTOR = 0.6088
+
+plt.close("all")
 
 # %% Read data
 raw_data_fn = Path(__file__).parent / "_data/raw.xlsx"
 df_bouys: pd.DataFrame = pd.read_excel(raw_data_fn, "buoys")
 df_stretches: pd.DataFrame = pd.read_excel(raw_data_fn, "stretches")
-
-
-def convert_coordinate(c: str) -> float:
-    """Convert coordinate string to float.
-
-    Input format: "52°30,0000' N"
-    """
-    new = (
-        c.replace("°", " ")
-        .replace("'", " ")
-        .replace('"', " ")
-        .replace(",", ".")
-    )
-    deg, minutes, direction = new.split()
-    return int(deg) + float(minutes) / 60.0
-
 
 # %% Normalize data
 df_bouys["name"] = (
@@ -61,28 +50,43 @@ df_stretches = pd.merge(
     suffixes=["_start", "_end"],
     how="left",
 )
-df_stretches["dx"] = (df_stretches["lon_start"] - df_stretches["lon_end"]) * 60
-df_stretches["dy"] = (df_stretches["lat_start"] - df_stretches["lat_end"]) * 60
+df_stretches["dx"] = (df_stretches["lon_end"] - df_stretches["lon_start"]) * 60
+df_stretches["dy"] = (df_stretches["lat_end"] - df_stretches["lat_start"]) * 60
 df_stretches["dx_nm"] = df_stretches["dx"] * LON_FACTOR
 df_stretches["dy_nm"] = df_stretches["dy"]
 df_stretches["dist"] = np.sqrt(
     df_stretches["dx_nm"] ** 2 + df_stretches["dy"] ** 2
 )
 df_stretches["distance_diff"] = df_stretches["Afstand"] - df_stretches["dist"]
+df_stretches["heading"] = get_direction(
+    df_stretches["dx_nm"], df_stretches["dy_nm"]
+)
+df_stretches["reverse_heading"] = get_direction(
+    -df_stretches["dx_nm"], -df_stretches["dy_nm"]
+)
 
+
+# Limit to Ijsselmeer
+df = df_stretches.query("Zone_start == 'Ijsselmeer' | Zone_end == 'Ijsselmeer'")
 
 # %% Plots
-df_stretches_plot = df_stretches.dropna(axis="index")
-n_total = len(df_stretches)
-n_found = len(df_stretches_plot)
+df_plot = df.dropna(axis="index")
+n_total = len(df)
+n_found = len(df_plot)
 
 print(f"{n_total=}, {n_found=}, n_not_found={n_total-n_found}")
 
 # Plot points and edges
-ax = sns.scatterplot(data=df_bouys, x="lon", y="lat", style="Zone", hue="Zone")
+ax = sns.scatterplot(
+    data=df_bouys.query("Zone == 'Ijsselmeer'"),
+    x="lon",
+    y="lat",
+    style="Zone",
+    hue="Zone",
+)
 ax.set_aspect(1 / LON_FACTOR)
 
-for i, r in df_stretches_plot.iterrows():
+for i, r in df_plot.iterrows():
     sx, sy = r["lon_start"], r["lat_start"]
     ex, ey = r["lon_end"], r["lat_end"]
     ax.plot([sx, ex], [sy, ey])
@@ -90,8 +94,8 @@ for i, r in df_stretches_plot.iterrows():
 # Plot difference between calculated and official distance
 plt.figure()
 ax = sns.scatterplot(
-    data=df_stretches_plot,
-    x=df_stretches_plot.index,
+    data=df_plot,
+    x=df_plot.index,
     y="distance_diff",
     style="Zone_start",
     hue="Zone_end",
