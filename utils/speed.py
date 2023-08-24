739bc927ce8eceb6328cd436129ed7025e62d8c4
Dima Pustakhod <dima.pu@gmail.com>
2023-08-24 17:31:47 +0200

    Add alternative speed model

--- utils/speed.py
+++ utils/speed.py
@@ -8,16 +8,65 @@ SPEED = [0, 0, 0, 0, 4, 5, 3, 2.5]
 SPEED_SPLINE = PchipInterpolator(BEARINGS, SPEED)
 
 
+COURSE_SPEED_DATA_ALEX = [
+    (0, 0.1),
+    (10, 0.1),
+    (20, 0.1),
+    (30, 0.1),
+    (40, 4.5),
+    (50, 5),
+    (60, 5),
+    (70, 5.5),
+    (80, 6),
+    (90, 6),
+    (100, 5.5),
+    (110, 5),
+    (120, 4.5),
+    (130, 4),
+    (140, 4),
+    (150, 3.5),
+    (160, 3.5),
+    (170, 3.5),
+    (180, 3.5),
+    # (190, 3.5),
+    # (200, 3.5),
+    # (210, 3.5),
+    # (220, 4),
+    # (230, 4),
+    # (240, 4.5),
+    # (250, 5),
+    # (260, 5.5),
+    # (270, 6),
+    # (280, 6),
+    # (290, 5.5),
+    # (300, 5),
+    # (310, 5),
+    # (320, 4.5),
+    # (330, 0.1),
+    # (340, 0.1),
+    # (350, 0.1),
+    # (360, 0.1)
+]
+BEARINGS_ALEX = [s[0] for s in COURSE_SPEED_DATA_ALEX]
+SPEED_ALEX = [s[1] for s in COURSE_SPEED_DATA_ALEX]
+SPEED_SPLINE_ALEX = PchipInterpolator(BEARINGS_ALEX, SPEED_ALEX)
+
+
 def calculate_pointing_angle(coarse: float, wind_direction: float) -> float:
-    assert 0 <= wind_direction < 360, "wind direction must be in [0, 360)" 
     
-    pointing_angle =  np.abs(- np.abs((coarse - wind_direction) - 180) + 180)
+    assert 0 <= wind_direction < 360, "wind direction must be in [0, 360)"
+
+    pointing_angle = np.abs(-np.abs((coarse - wind_direction) - 180) + 180)
     pointing_angle = -np.abs(-(pointing_angle - 180)) + 180
-    
+
     return pointing_angle
 
 
-def get_speed(coarse: float, wind_direction: float) -> float:
+MODELS = {'alex': SPEED_SPLINE_ALEX, 
+          "dima": SPEED_SPLINE}
+
+
+def get_speed(coarse: float, wind_direction: float, model="alex") -> float:
     """Calculate SOG for given coarse and wind.
 
     Wind direction is where the wind is coming from.
@@ -28,15 +77,16 @@ def get_speed(coarse: float, wind_direction: float) -> float:
     non-zero speed, although smaller than @ close-haul.
 
     """
+    spline = MODELS[model]
     pointing_angle = calculate_pointing_angle(coarse, wind_direction)
 
-    speed = SPEED_SPLINE(pointing_angle)
+    speed = spline(pointing_angle)
     return speed
 
 
 def plot_pointing_angle_profile(wind=0):
     plt.figure()
-    
+
     coarse = np.linspace(0, 360)
     plt.plot(coarse, calculate_pointing_angle(coarse, wind))
     plt.xticks([0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360])
@@ -45,14 +95,18 @@ def plot_pointing_angle_profile(wind=0):
     plt.ylabel(f"Pointing angle (0 = upwind, 180 = downwind). Wind is {wind}")
 
 
-
 def plot_speed_profile():
     plt.figure()
-    
-    wind = 90
-    
+
+    wind = 0
+
     coarse = np.linspace(0, 360, 360)
-    plt.plot(coarse, get_speed(coarse, wind))
+    
+    plt.plot(coarse, get_speed(coarse, wind, model="alex"), label="alex")
+    plt.plot(BEARINGS_ALEX, SPEED_ALEX, ".", label="alex_data")
+    plt.plot(coarse, get_speed(coarse, wind, model="dima"), label="dima")
+    plt.plot(BEARINGS, SPEED, ".", label="dima_data")
+    
     plt.xticks([0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360])
     plt.grid(True)
     # plt.xlabel("Coarse. Pointing angle (0 = upwind, 180 = downwind)")
@@ -62,7 +116,7 @@ def plot_speed_profile():
 
 def main():
     plt.close("all")
-    # plot_speed_profile()
+    plot_speed_profile()
     plot_pointing_angle_profile(340)
 
 
